8bc2afe89e2dfe8355d14c89747b836c3a259eeb
TsuchiyaTatsuki <kakakikiup1@gmail.com>
2018-04-24 11:59:32 +0900

    no message

---
+++ Project/src/beans/ItemDataBeans.java
@@ -0,0 +1,70 @@
+package beans;
+
+import java.io.Serializable;
+
+public class ItemDataBeans implements Serializable {
+	private int id;
+	private String name;
+	private int gender;
+	private int category;
+	private String detail;
+	private int price;
+	private String fileName;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int itemId) {
+		this.id = itemId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String itemName) {
+		this.name = itemName;
+	}
+
+	public String getDetail() {
+		return detail;
+	}
+
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int itemPrice) {
+		this.price = itemPrice;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String filename) {
+		this.fileName = filename;
+	}
+
+	public int getGender() {
+		return gender;
+	}
+
+	public void setGender(int gender) {
+		this.gender = gender;
+	}
+
+	public int getCategory() {
+		return category;
+	}
+
+	public void setCategory(int category) {
+		this.category = category;
+	}
+
+}
---
+++ Project/src/ecsite/Login.java
@@ -0,0 +1,55 @@
+package ecsite;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Login() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//ログイン失敗時に使用するため
+		HttpSession session = request.getSession();
+		String inputLoginId = session.getAttribute("loginId") != null
+				? (String) EcHelper.cutSessionAttribute(session, "loginId")
+				: "";
+		String loginErrorMessage = (String) EcHelper.cutSessionAttribute(session, "loginErrorMessage");
+
+		request.setAttribute("inputLoginId", inputLoginId);
+		request.setAttribute("loginErrorMessage", loginErrorMessage);
+
+		request.getRequestDispatcher(EcHelper.LOGIN_PAGE).forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
